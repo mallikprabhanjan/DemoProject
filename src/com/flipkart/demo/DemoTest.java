305faff86d11594104795448ba1b7d0565fd5bea
mallikprabhanjan <vemula.prabhanjan@flipkart.com>
2024-01-23 15:46:50 +0530

    First commit

---
+++ src/com/flipkart/demo/DemoTest.java
@@ -0,0 +1,14 @@
+package com.flipkart.demo;
+
+public class DemoTest {
+
+	public DemoTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first application");
+	}
+
+}
